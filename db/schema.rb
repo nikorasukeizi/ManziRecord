919fc4dc146f5ea2765d5e2694d5b929504585c1
kochiromatsumoto <tandm.516@gmail.com>
2019-05-14 04:44:37 +0000

    [add]deviseにて新規登録、ログイン機能追加(ビューは未着手)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_073955) do
+ActiveRecord::Schema.define(version: 2019_05_14_042614) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -90,20 +90,30 @@ ActiveRecord::Schema.define(version: 2019_05_13_073955) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "rubi_first_name"
     t.string "rubi_last_name"
-    t.string "postcord"
+    t.string "postcode"
     t.text "address"
     t.string "tel"
-    t.string "email"
-    t.string "password"
-    t.date "birthdate"
+    t.date "barthdate"
     t.boolean "status"
     t.boolean "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
