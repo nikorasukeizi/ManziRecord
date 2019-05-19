c0aa4f0be1fb36dc6183117c622d48d18fb656ad
panchotky <panchotkycareer@gmai.com>
2019-05-19 12:43:25 +0900

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,14 +13,14 @@ class ItemsController < ApplicationController
   end
 
   def index
-      @genre = Genre.find(params[:id])
+      # @genre = Genre.find(params[:id])
 
-      if @genre = nil?
+      # if @genre = nil?
          @items = Item.all
 
-      else
-       @genreitems = @genre.item_id.all
-      end
+      # else
+       # @genreitems = @genre.item_id.all
+      # end
   end
 
   def search_result
