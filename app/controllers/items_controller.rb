c13a7295cd1b62d2de0ed411de21068cebc8a6ec
panchotky <panchotkycareer@gmai.com>
2019-05-19 18:11:09 +0900

    [Add]ALGの編集画面

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,7 +56,7 @@ class ItemsController < ApplicationController
 
   def update
       item = Item.find(params[:id])
-      item.update
+      item.update(item_params)
       redirect_to item_path(item.id)
   end
 
