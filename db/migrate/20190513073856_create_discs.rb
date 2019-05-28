5d701a94127ef2521ce947cea64b473c917180f9
stysk0209 <stysk0209@outlook.jp>
2019-05-28 18:25:20 +0900

    [Fix]product環境用記述追加

--- db/migrate/20190513073856_create_discs.rb
+++ db/migrate/20190513073856_create_discs.rb
@@ -1,6 +1,6 @@
 class CreateDiscs < ActiveRecord::Migration[5.2]
   def change
-    create_table :discs do |t|
+    create_table :discs, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.integer :item_id
       t.string :name
 
